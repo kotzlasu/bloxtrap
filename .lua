d6bf1e110f552ef76cd309ed5628e52c77e42155
kotzlasu <166518974+kotzlasu@users.noreply.github.com>
2025-01-13 20:29:15 +0100

    Update .lua

--- .lua
+++ .lua
@@ -167,7 +167,7 @@ end)
 -- Toggle GUI with "K" Key
 local UserInputService = game:GetService("UserInputService")
 UserInputService.InputBegan:Connect(function(input, gameProcessed)
-    if input.KeyCode == Enum.KeyCode.q then
+    if input.KeyCode == Enum.KeyCode.0 then
         ScreenGui.Enabled = not ScreenGui.Enabled
     end
 end)
